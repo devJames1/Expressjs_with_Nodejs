187a7426b04e494d5e199dfdacc9c54949ea0ca0
James Okolie <zebedee788@gmail.com>
2023-05-10 23:45:22 +0400

    ExpressJS routing

---
+++ script.js
@@ -0,0 +1,27 @@
+// Simple ExpressJS App
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.end('Hello, World!')
+// })
+
+// // app.get('/h', (req, res) => {
+// //     res.end('Hello, World H!')
+// // }) 
+
+// app.listen(8000, () => console.log('Listening on port 8000'))
+
+
+//ExpressJS Routing
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => res.end('Hompage'))
+// app.get('/about', (req, res) => res.end('About Us'))
+// app.get('/services', (req, res) => res.end('Services'))
+// app.get('/help', (req, res) => res.end('Help'))
+
+
+
+// app.listen(8000, () => console.log('Listening on port 8000'))
