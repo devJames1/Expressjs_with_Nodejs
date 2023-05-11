cd46fce86cab7c06e0556fae5210c1a4a30d6125
James Okolie <zebedee788@gmail.com>
2023-05-11 14:21:18 -0700

    expressjs advanced routing added

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,55 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => {
+    //geting parameters passed directly into the query url e.g http://localhost:8000/users?name=Jose
+    console.log(req.query.name)
+
     res.send('User List')
 }) 
 
 router.get('/new', (req, res) => {
-    res.send('User New Form')
-}) 
+    // res.send('User New Form')
+    res.render('users/new')
+})  
 
 router.post('/', (req, res) => {
-    res.send('Create User')
+    // res.send('Create User')
+    //code to emulate real world scenerio
+    const isValid = false
+    if (isValid) {
+        users.push({ firstName: req.body.firstName })
+        res.redirect(`/users/${users.length - 1}`)
+    }else{
+        console.log('Error')
+        res.render('users/new', {firstName: req.body.firstName })
+    }
+    // console.log(req.body.firstName)
+    // res.send('Hi')
 })
 
-router.get('/new', (req, res) => {
-    res.send('User New Form')
-}) 
+// creating a dynamic url route that routes to anything of the same string structure e.g users/1, users/2 ...users/:'anthing',  
+//router.route helps us link similar request like get,put,delete
+router.route('/:id').get( (req, res) => {
+    // this gets the id dynamically
+    // console.log(req.user)
+    console.log(req.user)
+    res.send(`Get User with ID ${req.params.id}`)
+}).put((req, res) => {
+    // this gets the id dynamically
+    req.params.id
+    res.send(`Update User with ID ${req.params.id}`)
+}).delete((req, res) => {
+    // this gets the id dynamically
+    req.params.id
+    res.send(`Delete User with ID ${req.params.id}`)
+})
 
+//param in express it runs anytime it finds a param that matches a stringe.g id, next should be defind else it's stuck infinately. Param is likea middleware
+// saves us from writing ton of code inside the different request above to get the user, it allows us do it in one place.  
+const users = [{name: 'Kyle'}, {name: 'Sally'}]
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
 
 module.exports = router
-
--- second_test_script.js
+++ second_test_script.js
@@ -2,16 +2,38 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(logger)
 
-app.get('/', (req, res) => {
-    console.log('root route')
-    res.render('index2.ejs', {text22: 'World'})
-})
+//MIDDLEWARE TO ALLOW US ACCESS BODY OF REQUEST IN EXPRESS BY DEFAULT EXPRESS DON"T ALLOW ACCESS TO BODY, SO USE MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+
+//FOR JSON REQUEST ALWAYS ADD THIS LINE this allows you to parse json information from the body of requestt/response 
+app.use(express.json()) 
+
+//USING A MIDDLEWARE STATIC HTML THAT NEVER CHANGES
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//     // console.log('root route')
+//     res.render('index2.ejs', {text22: 'World'})
+// })
 
 //import the users file and use
 const userRouter = require('./routes/users')
 
-// make /users the base route for all user related request.
+// make /users the base route for all user related request..
 app.use('/users', userRouter)
+
+// MIDLLEWARE BASICS:
+
+//To use only in a particular route define inside the route like so: app.get('/', logger (req, res) => {}).
+//calling a middle ware functions multiple times will run it multiple times, it is usefull when yyou want multiple functionsto run before returning output to user. e.g app.get('/', logger, logger, logger (req, res) => {})
+// as long as you call next it will run the next code and only on that route
+
+// you can also define middlewares functions in the seperate route files to only run them on that route
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 app.listen(8000, () => console.log('Listening on port 8000'))
